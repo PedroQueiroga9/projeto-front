abb49b081c8827e6a49fe4ceaa8cca82842df248
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2026-02-07 13:45:26 -0300

    fonecedores

--- sistema-gestao/src/pages/Fornecedores.jsx
+++ sistema-gestao/src/pages/Fornecedores.jsx
@@ -1,18 +1,40 @@
-import { Button } from 'react-bootstrap'
-import React from 'react'
+import { Button, Card, Col, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Fornecedores = () => {
+
+    const [fornecedores, setFornecedores] = useState([])
+
+    useEffect(() => { carregarFornecedores }, [])
+
+    const carregarFornecedores = () => {
+        axios.get("http://localhost:3000/fornecedores")
+            .then(response => setFornecedores(response.data))
+            .catch(error => console.error("Erro a carregar fornecedores", error))
+    }
+
     return (
         <div>
             <div className="d=-flex justify-content-between align-items-center mb-4">
                 <h2 className="mb-0 text-white">
-                    <i className="bi bi-building me-2"></i>
                     Fornecedores
                 </h2>
-                <Button variant='light'> 
-                    Novo Fornecedor 
+                <Button variant='light'>
+                    <i className="bi bi-building me-2"></i>
+                    Novo Fornecedor
                 </Button>
+
             </div>
+            <Row className='g-4'>
+                {
+                fornecedores.map(fornecedor => (
+                    <Col md={6} lg={4} key={fornecedor.id}>
+                            <Card className="h-100 shadow-sm border-0"> </Card>
+                    </Col>
+                ))
+                }
+            </Row>
         </div>
     )
 }
